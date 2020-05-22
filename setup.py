1a3e187dcdd586a68778f5b495f41c00f8567bfb
Felix Stein <mail@flxn.de>
2020-05-22 11:53:24 +0200

    basic setup.py structure for packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='django-csp-observer',
+   version='0.1',
+   description='CSP observer app for Django',
+   author='Felix Stein',
+   author_email='mail@flxn.de',
+   packages=['csp_observer'],
+   #install_requires=['bar', 'greek'],
+)
